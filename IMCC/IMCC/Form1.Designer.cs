239af6b56ce01aebff0f77b2d666d2863ddded41
Nicolas C L Barletta <58794347+Nickgw25@users.noreply.github.com>
2021-01-02 15:21:28 -0300

    Add files via upload

---
+++ IMCC/IMCC/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace IMCC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(285, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular IMC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 317);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ IMCC/IMCC/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double A, P,IMC;
+            
+            A = Convert.ToDouble(textBox1.Text);
+            P = Convert.ToDouble(textBox2.Text);
+            IMC = P / (A *A);
+            textBox3.Text = IMC.ToString("0.00");
+           
+            if (IMC < 18.49) 
+                MessageBox.Show("Situação: Você está abaixo do Peso", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            
+            else if (IMC <= 24.99)  
+                    MessageBox.Show("Situação: Você está com Peso na Normalidade", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                
+            else if (IMC < 29.99) 
+                MessageBox.Show("Situação: Você está acima do Peso", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+            else if (IMC < 34.99) 
+                MessageBox.Show("Situação: Você está com Obesidade Grau I", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            
+            else if (IMC < 39.99) 
+                MessageBox.Show("Situação: Você está com Obesidade Grau II (Severa)", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            
+            else if (IMC > 40) 
+                MessageBox.Show("Situação: Você está com Obesidade Grau III (Mórbida)", "Cálculo IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            
+
+        }
+    }
+}
